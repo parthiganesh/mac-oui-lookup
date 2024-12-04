dd93b75a7a72a4f85c1869d60ce87316251a021a
parthiganesh <whitespura@gmail.com>
2024-12-04 21:02:01 +0400

    download csv one-by-one

--- src/utils.js
+++ src/utils.js
@@ -3,9 +3,9 @@ const csvParser = require('csv-parser');
 const fs = require('fs');
 
 const urls = [
-  'http://standards-oui.ieee.org/oui/oui.csv',
-  'http://standards-oui.ieee.org/oui28/mam.csv',
-  'http://standards-oui.ieee.org/oui36/oui36.csv'
+  'https://standards-oui.ieee.org/oui/oui.csv',
+  'https://standards-oui.ieee.org/oui28/mam.csv',
+  'https://standards-oui.ieee.org/oui36/oui36.csv'
 ];
 const outputFile = './src/vendor-data.json';
 
@@ -14,23 +14,18 @@ let oui_count = 0;
 
 function processCsvFile(url) {
   return new Promise((resolve, reject) => {
-
-    rejectWithError = (e) => reject(e);
     console.log(`Downloading ${url}`);
-    
+
     request.get(url)
-      .on('error', rejectWithError)
-
+      .on('error', reject)
       .pipe(csvParser())
-      .on('error', rejectWithError)
-
+      .on('error', reject)
       .on('data', (data) => {
         const oui = data['Assignment'];
         const vendorName = data['Organization Name'];
         vendorData[oui] = vendorName;
         process.stdout.write(`${oui_count++} \r`);
       })
-
       .on('end', () => {
         console.log(`Processed ${url}`);
         resolve();
@@ -38,12 +33,18 @@ function processCsvFile(url) {
   });
 }
 
-Promise.all(urls.map(processCsvFile))
-  .then((result) => {
+async function downloadAndProcessFiles() {
+  try {
+    for (let url of urls) {
+      await processCsvFile(url); // Wait for the current file to be processed before moving to the next
+    }
+
     console.log(`Writing ${oui_count} OUIs`);
     fs.writeFileSync(outputFile, JSON.stringify(vendorData, Object.keys(vendorData).sort(), 2));
     console.log(`${outputFile} created successfully`);
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error('Error:', err);
-  });
+  }
+}
+
+downloadAndProcessFiles();
